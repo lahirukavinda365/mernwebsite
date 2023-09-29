7bbe75394454c7747fd22c7d08053a3d5c2d84d1
Lahiru Karunarathna <lahirukavinda365@gmail.com>
2023-09-29 12:29:00 +0530

    frontend initialized, navbar component created.

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+  <nav className="navbar navbar-expand-lg navbar-light shadow">
+  <div className="container">
+    
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contacts</a>
+        </li>
+        {/* <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> */}
+        {/* <li className="nav-item">
+          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li> */}
+      </ul>
+      <a className='navbar-brand fw-bolder fs-4 mx-auto' href='#'>Navbar</a>
+      <button className="btn btn-outline-primary ms-auto px-4 rounded-pill"><i className='fa fa-sign-in me-2'></i> Login</button>
+      <button className="btn btn-outline-primary ms-auto px-4 rounded-pill"><i className='fa fa-user-plus me-2'></i>Register</button>
+
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar   
