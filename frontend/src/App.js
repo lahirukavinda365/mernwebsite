0bf111a22275bfcdddd2a45ada9bb397f3b0d0e8
Lahiru Karunarathna <lahirukavinda365@gmail.com>
2023-09-29 16:50:09 +0530

    Services component completed.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Services />
     </>
   );
 }
